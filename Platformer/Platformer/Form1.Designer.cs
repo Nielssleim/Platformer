1d235a59e2bbb6509e61d677b5e27eb54345d3ae
DESKTOP-3PRQ9PH\Kakalakoemba <gamma123gamma@gmail.com>
2018-09-19 21:09:12 +0200

    commit first code

---
+++ Platformer/Platformer/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Platformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Base = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Screen1 = new System.Windows.Forms.Panel();
+            this.life1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.wall = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.gameOver = new System.Windows.Forms.Label();
+            this.lifesFrame = new System.Windows.Forms.Label();
+            this.Ouch = new System.Windows.Forms.PictureBox();
+            this.Blok2 = new System.Windows.Forms.PictureBox();
+            this.Blok = new System.Windows.Forms.PictureBox();
+            this.Poppetje = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Screen1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.life1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ouch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blok2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blok)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Poppetje)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Base
+            // 
+            this.Base.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Base.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Base.Location = new System.Drawing.Point(0, 641);
+            this.Base.Name = "Base";
+            this.Base.Size = new System.Drawing.Size(1815, 128);
+            this.Base.TabIndex = 1;
+            this.Base.Paint += new System.Windows.Forms.PaintEventHandler(this.Base_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Screen1
+            // 
+            this.Screen1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Screen1.Controls.Add(this.life1);
+            this.Screen1.Controls.Add(this.pictureBox1);
+            this.Screen1.Controls.Add(this.wall);
+            this.Screen1.Controls.Add(this.enemy1);
+            this.Screen1.Controls.Add(this.gameOver);
+            this.Screen1.Controls.Add(this.lifesFrame);
+            this.Screen1.Controls.Add(this.Ouch);
+            this.Screen1.Controls.Add(this.Blok2);
+            this.Screen1.Controls.Add(this.Blok);
+            this.Screen1.Controls.Add(this.Poppetje);
+            this.Screen1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Screen1.Location = new System.Drawing.Point(0, 0);
+            this.Screen1.Name = "Screen1";
+            this.Screen1.Size = new System.Drawing.Size(1815, 646);
+            this.Screen1.TabIndex = 2;
+            // 
+            // life1
+            // 
+            this.life1.Image = global::Platformer.Properties.Resources.life;
+            this.life1.Location = new System.Drawing.Point(4, 615);
+            this.life1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.life1.Name = "life1";
+            this.life1.Size = new System.Drawing.Size(30, 31);
+            this.life1.TabIndex = 3;
+            this.life1.TabStop = false;
+            this.life1.Click += new System.EventHandler(this.life1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(-44, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2, 657);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // wall
+            // 
+            this.wall.BackColor = System.Drawing.Color.Black;
+            this.wall.Dock = System.Windows.Forms.DockStyle.Left;
+            this.wall.Location = new System.Drawing.Point(0, 0);
+            this.wall.Name = "wall";
+            this.wall.Size = new System.Drawing.Size(2, 646);
+            this.wall.TabIndex = 6;
+            this.wall.TabStop = false;
+            this.wall.Visible = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.BackColor = System.Drawing.Color.Yellow;
+            this.enemy1.Location = new System.Drawing.Point(156, 622);
+            this.enemy1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(27, 25);
+            this.enemy1.TabIndex = 5;
+            this.enemy1.TabStop = false;
+            this.enemy1.Move += new System.EventHandler(this.enemy1_move);
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.Location = new System.Drawing.Point(732, 285);
+            this.gameOver.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(0, 20);
+            this.gameOver.TabIndex = 4;
+            this.gameOver.Paint += new System.Windows.Forms.PaintEventHandler(this.label2_Create);
+            // 
+            // lifesFrame
+            // 
+            this.lifesFrame.AutoSize = true;
+            this.lifesFrame.Location = new System.Drawing.Point(18, 14);
+            this.lifesFrame.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lifesFrame.Name = "lifesFrame";
+            this.lifesFrame.Size = new System.Drawing.Size(51, 20);
+            this.lifesFrame.TabIndex = 3;
+            this.lifesFrame.Text = "label1";
+            this.lifesFrame.Paint += new System.Windows.Forms.PaintEventHandler(this.label1_Create);
+            // 
+            // Ouch
+            // 
+            this.Ouch.BackColor = System.Drawing.Color.Red;
+            this.Ouch.Location = new System.Drawing.Point(1401, 622);
+            this.Ouch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Ouch.Name = "Ouch";
+            this.Ouch.Size = new System.Drawing.Size(250, 60);
+            this.Ouch.TabIndex = 3;
+            this.Ouch.TabStop = false;
+            this.Ouch.Click += new System.EventHandler(this.Ouch_Click);
+            this.Ouch.Paint += new System.Windows.Forms.PaintEventHandler(this.Ouch_Create);
+            // 
+            // Blok2
+            // 
+            this.Blok2.BackColor = System.Drawing.Color.Transparent;
+            this.Blok2.BackgroundImage = global::Platformer.Properties.Resources.fan;
+            this.Blok2.Location = new System.Drawing.Point(927, 326);
+            this.Blok2.Name = "Blok2";
+            this.Blok2.Size = new System.Drawing.Size(210, 320);
+            this.Blok2.TabIndex = 2;
+            this.Blok2.TabStop = false;
+            this.Blok2.Click += new System.EventHandler(this.Blok2_Click);
+            // 
+            // Blok
+            // 
+            this.Blok.BackColor = System.Drawing.Color.Black;
+            this.Blok.Location = new System.Drawing.Point(632, 485);
+            this.Blok.Name = "Blok";
+            this.Blok.Size = new System.Drawing.Size(100, 172);
+            this.Blok.TabIndex = 1;
+            this.Blok.TabStop = false;
+            this.Blok.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Poppetje
+            // 
+            this.Poppetje.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Poppetje.Location = new System.Drawing.Point(728, 29);
+            this.Poppetje.Name = "Poppetje";
+            this.Poppetje.Size = new System.Drawing.Size(30, 25);
+            this.Poppetje.TabIndex = 0;
+            this.Poppetje.TabStop = false;
+            this.Poppetje.Click += new System.EventHandler(this.Poppetje_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1815, 769);
+            this.Controls.Add(this.Screen1);
+            this.Controls.Add(this.Base);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Screen1.ResumeLayout(false);
+            this.Screen1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.life1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ouch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blok2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Poppetje)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel Base;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel Screen1;
+        private System.Windows.Forms.PictureBox Poppetje;
+        private System.Windows.Forms.PictureBox Blok;
+        private System.Windows.Forms.PictureBox Blok2;
+        private System.Windows.Forms.PictureBox Ouch;
+        private System.Windows.Forms.Label lifesFrame;
+        private System.Windows.Forms.Label gameOver;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox wall;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox life1;
+    }
+}
+
---
+++ Platformer/Platformer/Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Platformer
+{
+
+    public partial class Form1 : Form
+    {   int lifes = 5;
+        int Zwaartekracht;
+        int Kracht = 15;
+        int dammageKracht = 7;
+        bool rightL;
+        bool leftL;
+        bool Jump;
+        bool jumping;
+        bool onBlock = false;
+        bool onFan = false;
+        bool onLava = false;
+        bool onLife = false;
+        bool pause = false;
+        bool restart = false;
+        bool enamyMoveR = true;
+        bool enamyMoveL = false;
+        bool health = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (enamyMoveR == true)
+            {
+                enemy1.Left += 5;
+            }
+
+            if (enamyMoveL == true)
+            {
+                enemy1.Left -= 5;
+            } 
+
+            if (lifes == 0)
+            {
+                gameOver.SetBounds(490, (Screen1.Height / 2), 100, 40); 
+                gameOver.Font = new Font("", 30);
+                gameOver.Text = "Game Over";
+                pause = true;
+                this.timer1.Stop();
+            }
+
+
+            // POPPETJE KAN LINKS EN REWCHTS NIET DOOR BLOK LOPEN
+            if (Poppetje.Right > Blok.Left && Poppetje.Left < Blok.Right - Poppetje.Width / 2 && Poppetje.Bottom > Blok.Top)
+            {
+                rightL = false;
+            } else if (rightL == true)
+            {
+                Poppetje.Left = Poppetje.Left + 5;
+            }
+
+            if (Poppetje.Left < Blok.Right && Poppetje.Right > Blok.Left + Poppetje.Width / 2 && Poppetje.Bottom > Blok.Top)
+            {
+                leftL = false;
+            } else if (leftL == true)
+            {
+                Poppetje.Left = Poppetje.Left - 5;
+            }
+
+
+            // enemy1 KAN LINKS EN RECHTS NIET DOOR BLOK LOPEN
+            if (enemy1.Right > Blok.Left && enemy1.Left < Blok.Right - enemy1.Width / 2 && enemy1.Bottom > Blok.Top)
+            {
+                enamyMoveR = false;
+            }
+            else if (enamyMoveR == true)
+            {
+                enemy1.Left = enemy1.Left + 5;
+            }
+
+            if (enemy1.Left < Blok.Right && enemy1.Right > Blok.Left + enemy1.Width / 2 && enemy1.Bottom > Blok.Top)
+            {
+                enamyMoveL =  true;
+            }
+            else if (enamyMoveL == false)
+            {
+                enemy1.Left = enemy1.Left - 5;
+            }
+
+            // enemy1 ouch
+            if (enemy1.Left <= Poppetje.Left + (Poppetje.Width / 2) && enemy1.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {            
+                if (!jumping && !enemy1.IsDisposed)
+                {
+                    lifes--;
+                    lifesFrame.Text = "Lifes: " + lifes;
+                }
+                jumpenDammage();
+            }
+
+            // enemy1 kill
+            if (enemy1.Left <= Poppetje.Left + (Poppetje.Width / 2) && enemy1.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {
+                
+            }
+
+
+            // invisable wall  left
+            if (enemy1.Left < wall.Right && enemy1.Right > wall.Left + enemy1.Width / 2 && enemy1.Bottom > wall.Top)
+            {
+                enamyMoveR = true;
+            }
+            else if (enamyMoveL == true)
+            {
+                enemy1.Left = enemy1.Left - 5;
+            }
+
+
+            // GROND
+            if (Jump == true)
+            {
+                onBlock = false;
+                Poppetje.Top = Poppetje.Top - Zwaartekracht;
+                Zwaartekracht--;
+                
+                if (jumping == false)
+                {
+                    onBlock = true;
+                }
+                
+            }
+
+
+            // stopt poppetje van door de bodem vallen
+            if (Poppetje.Bottom >= Base.Top)
+            {
+                Poppetje.Top = Screen1.Height - Poppetje.Height;
+            }
+
+
+
+            // BLOK 1
+            if (Blok.Left <= Poppetje.Left + (Poppetje.Width / 2) && Blok.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {
+                if (onBlock == true)
+                {
+                    Poppetje.Top = Screen1.Height - Blok.Height - Poppetje.Height + 7;
+                }
+            }
+
+
+     
+            // BLOK 2 (ventilator)
+            if (Blok2.Left <= Poppetje.Left + (Poppetje.Width / 2) && Blok2.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {
+                onFan = true;
+                Poppetje.Top = Screen1.Height - Blok2.Height - Poppetje.Height;
+            }
+            if (!(Blok2.Left <= Poppetje.Left + (Poppetje.Width / 2) && Blok2.Right >= Poppetje.Right - (Poppetje.Width / 2)))
+            {
+                onFan = false;
+            }
+
+
+
+            //Ouch
+            if (Ouch.Left <= Poppetje.Left + (Poppetje.Width / 2) && Ouch.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {
+                if (!jumping && onLava == true)
+                {
+                    lifes--;
+                    lifesFrame.Text = "Lifes: " + lifes;
+                }
+
+                if (onLava == false && !jumping)
+                {
+                    lifes--;
+                    lifesFrame.Text = "Lifes: " + lifes;
+                    onLava = true;
+                }
+                jumpen();
+            }
+
+            if (!(Ouch.Left <= Poppetje.Left + (Poppetje.Width / 2) && Ouch.Right >= Poppetje.Right - (Poppetje.Width / 2)))
+            {
+                onLava = false;
+            }
+
+
+            
+
+            // life + 1
+            if (life1.Left <= Poppetje.Left + (Poppetje.Width / 2) && life1.Right >= Poppetje.Right - (Poppetje.Width / 2))
+            {
+                if (onLife == false && !life1.IsDisposed)
+                {
+                    lifes++;
+                    lifesFrame.Text = "Lifes: " + lifes;
+                    life1.Dispose();
+                    onLife = true;
+                }
+            }
+
+            if (!(life1.Left <= Poppetje.Left + (Poppetje.Width / 2) && life1.Right >= Poppetje.Right - (Poppetje.Width / 2)))
+            {
+                onLife = false;
+            }
+        }
+
+
+        //code for jumping 
+
+        private void jumpen()
+        {
+            if (jumping == false)
+            {
+                if (onFan == false)
+                {
+                    Jump = true;
+                    Zwaartekracht = Kracht;
+                    jumping = true;
+                    Thread jumpThread = new Thread(new ThreadStart(WorkThreadFunction));
+                    jumpThread.Start();
+                }
+            }
+        }
+
+        //code for jumping Dammage from enemy
+        private void jumpenDammage()
+        {
+            if (jumping == false)
+            {
+                Jump = true;
+                Zwaartekracht = dammageKracht;
+                jumping = true;
+                Thread jumpThread = new Thread(new ThreadStart(WorkThreadFunction));
+                jumpThread.Start();
+            }
+        }
+
+        //keyboard binding
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                rightL = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                leftL = false;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Poppetje.Top = Screen1.Height - Poppetje.Height;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //poppetje
+        private void Poppetje_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Base_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Poppetje_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                rightL = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                leftL = true;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                jumpen();
+            }
+        }
+
+        public void Ouch_Click(object sender, System.EventArgs e)
+        {
+
+        }
+        // to make the falling correct
+        public void WorkThreadFunction()
+        {
+            System.Threading.Thread.Sleep(500);
+            jumping = false;
+        }
+
+        // label That shows how many lifes the player has.
+        private void label1_Create(object sender, PaintEventArgs e)
+        {
+            lifesFrame.Height = 40;
+            lifesFrame.Width = 100;
+            lifesFrame.Font = new Font("", 30);
+            lifesFrame.Text = "Lifes: " + lifes;
+        }
+
+        private void Ouch_Create(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Create(object sender, PaintEventArgs e)
+        {
+         
+        }
+
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            /*
+            if (restart == true)
+            {
+                this.Close();
+            }
+            */
+        }
+        
+
+        private void enemy1_move(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Blok2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void life1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
